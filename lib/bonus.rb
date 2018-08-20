3651fd172e3c67ab07d1467dd3301300ab970d40
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-20 15:51:23 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,9 @@ def bonus
 
   #code your solution here:
 
-  epic_tragedy[:status] = "dead"
+  rom = epic_tragedy[hero] 
+  jul = epic_tragedy[heroine]
+  rom[:status] = "dead"
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
